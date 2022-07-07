ec353d8dc726835e08a1fd352467374034cfca1d
odo2063 <odo2063@users.noreply.github.com>
2022-07-07 21:21:06 +0200

    Python 3.6 not supported

--- setup.py
+++ setup.py
@@ -138,7 +138,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
